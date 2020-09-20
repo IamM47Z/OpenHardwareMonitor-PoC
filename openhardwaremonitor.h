0709929510bed51724ea1795a37351e113e2176a
imM47Z <38143854+imM47Z@users.noreply.github.com>
2020-09-20 15:00:47 +0100

    fixing space

--- openhardwaremonitor.h
+++ openhardwaremonitor.h
@@ -1,4 +1,12 @@
-#include "openhardwaremonitor_defs.h"
+#define READ_MSR_IOCTL 0x9C402084
+#define WRITE_MSR_IOCTL 0x9C402088
+
+#pragma pack(1)
+struct WRITE_MSR_STRUCT
+{
+	unsigned long id;
+	unsigned long long value;
+};
 
 namespace ohm
 {
